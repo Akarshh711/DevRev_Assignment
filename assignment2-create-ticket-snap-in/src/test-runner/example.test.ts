d98e6e7c2e551b1df4ba66d0fb9cc4ca15157488
Akarshh711 <96855236+Akarshh711@users.noreply.github.com>
2024-04-12 12:43:47 +0530

    Add files via upload

---
+++ assignment2-create-ticket-snap-in/src/test-runner/example.test.ts
@@ -0,0 +1,17 @@
+import { run } from '../functions/ticket_creator';
+
+describe('Test some function', () => {
+  it('Something', () => {
+    run([
+      {
+        payload: {
+          work_created: {
+            work: {
+              id: 'some-id',
+            },
+          },
+        },
+      },
+    ]);
+  });
+});
